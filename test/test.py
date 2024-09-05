4c209dd72f280d7c7ba9c55b09230ef2e4a5638b
Yuri Panchul <yuri@panchul.com>
2024-09-04 20:17:43 -0700

    Improved the testbench

--- test/test.py
+++ test/test.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: © 2024 Tiny Tapeout
+# SPDX-FileCopyrightText: © 2024 Tiny Tapeout and Yuri Panchul
 # SPDX-License-Identifier: Apache-2.0
 
 import cocotb
@@ -23,24 +23,10 @@ async def test_project(dut):
     await ClockCycles(dut.clk, 10)
     dut.rst_n.value = 1
 
-    dut._log.info("Test project behavior")
+    dut._log.info("CPU starts program execution")
 
-    # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
-
-    # Wait for one clock cycle to see the output values
-    await ClockCycles(dut.clk, 1)
-
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
-
-    # YURI: I commented this out to avoid test failure
-    # assert dut.uo_out.value == 50
-
-    # Keep testing the module by changing the input values, waiting for
-    # one or more clock cycles, and asserting the expected output values.
-
-    # YURI: I added the extra wait below
-    # Wait for 300 clock cycles to see the output values
+    # Wait for 300 clock cycles necessary to verify the functionality
     await ClockCycles(dut.clk, 300)
+
+    # Check the test passed
+    assert dut.iuo_out.value == 1
